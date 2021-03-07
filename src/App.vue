c9007e72629bc9ac3283e3ce6acb98154b913619
Nathan Grant <apeling@gmail.com>
2021-03-06 19:31:47 -0500

    Add version

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
+  <p>{{version}}</p>
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup () {
+    return {
+      version: ref(process.env.VUE_APP_VERSION)
+    }
+  }
+}
+</script>
 
 <style>
 #app {
